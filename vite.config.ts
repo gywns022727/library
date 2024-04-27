7021c6b24eac45b2a9f07ef1659d9fda9e2a63ef
gywns022727 <lifford050227a@gmail.com>
2024-04-27 17:19:10 +0900

    update add vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ svgrOptions: {} })],
+  build: {
+    target: "esnext",
+  },
+
+  plugins: [
+    react(),
+    svgr({ svgrOptions: {} }),
+
+    {
+      name: "inject-html",
+      transformIndexHtml(html) {
+        return html.replace(
+          "%VITE_KAKAO_API_KEY%",
+          process.env.VITE_KAKAO_API_KEY || ""
+        );
+      },
+    },
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: "/src/" },
