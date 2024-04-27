1c4111938be6446dd41b77444171a174fc71a275
gywns022727 <lifford050227a@gmail.com>
2024-04-27 16:20:00 +0900

    update absolute path setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr({ svgrOptions: {} })],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src/" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+    ],
+  },
+});
