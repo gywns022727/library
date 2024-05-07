e237c1c6d70cb2b96ec632ff48747d147595e2ad
gywns022727 <lifford050227a@gmail.com>
2024-05-08 01:48:12 +0900

    update resolve infinite request

--- src/Hooks/useGeolocation/index.ts
+++ src/Hooks/useGeolocation/index.ts
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 
+// 현재 위도, 경도 좌포를 반환하는 함수
 const useGeolocation = () => {
   const [position, setPosition] = useState<GeolocationPosition | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const successCallback = (pos: GeolocationPosition) => setPosition(pos);
     const errorCallback = (err: GeolocationPositionError) =>
-      console.error(`Error(${err.code}): ${err.message}`);
+      setError(`Error(${err.code}): ${err.message}`);
 
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  }, []);
+    if (!position && !error)
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, [position, error]);
 
-  // position 값이 없을 시 서울시청의 위도, 경도 좌표 반환
   const currentPosition = {
     lat: position ? Number(position?.coords.latitude) : 37.5665,
     lng: position ? Number(position?.coords.longitude) : 126.978,
   };
 
+  // position 값이 없을 시 서울시청의 위도, 경도 좌표 반환
   return currentPosition;
 };
 
