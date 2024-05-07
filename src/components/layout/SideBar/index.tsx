b2d6a7838086487331a624f6a61924eb061635c3
gywns022727 <lifford050227a@gmail.com>
2024-05-08 01:49:07 +0900

    test useAddress Hook

--- src/components/layout/SideBar/index.tsx
+++ src/components/layout/SideBar/index.tsx
@@ -2,18 +2,25 @@ import { RootState } from "@/store/config";
 import { useSelector, useDispatch } from "react-redux";
 import { SET_EXPANSION } from "@/store/slices/sideBarSlice";
 
+import { useAddress } from "@/Hooks/useAddress";
+import { useGeolocation } from "@/Hooks/useGeolocation";
+
 import { RiArrowRightSFill } from "react-icons/ri";
 import { Container, ExpansionButton, SearchContainer } from "./styled";
 
 export default function SideBar() {
-  const { sideBar } = useSelector((state: RootState) => state.sideBar);
-  const dispatch = useDispatch();
+  const sideBar = useSelector((state: RootState) => state.sideBar.sideBar);
+  const currentPosition = useGeolocation();
+  const address = useAddress(currentPosition);
 
+  const dispatch = useDispatch();
   const handleSideBar = () => dispatch(SET_EXPANSION(!sideBar));
 
   return (
     <Container $sideBar={sideBar}>
       <SearchContainer>
+        <button onClick={() => console.log(address)}>asdasd</button>
+
         <div>
           <h3>Library</h3>
           <input type="text" />
